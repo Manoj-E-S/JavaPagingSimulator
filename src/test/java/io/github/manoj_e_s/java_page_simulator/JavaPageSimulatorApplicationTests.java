594004630c5fafd01a8bbbcef09798f61a69753d
Manoj E S <manoj.es.professional@gmail.com>
2024-09-28 22:14:39 +0530

    Fix auto-generated test directory to not include any tests (for now)

--- src/test/java/io/github/manoj_e_s/java_page_simulator/JavaPageSimulatorApplicationTests.java
+++ src/test/java/io/github/manoj_e_s/java_page_simulator/JavaPageSimulatorApplicationTests.java
@@ -1,12 +1,8 @@
 package io.github.manoj_e_s.java_page_simulator;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class JavaPageSimulatorApplicationTests {
 
-	@Test
+	// Test
 	void contextLoads() {
 	}
 
